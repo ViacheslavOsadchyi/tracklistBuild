0ca8d3b6f74a1f2bc4e5545e56605a050cc0b679
1010519rg46ft9o <slava.osadchyi.dev@gmail.com>
2019-05-01 20:04:04 +0300

    add assets

---
+++ precache-manifest.1aadd6460d960a94eb6184428f0ab032.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea43dfb5d983d4c94cebcc8073ecb143",
+    "url": "/tracklistBuild/index.html"
+  },
+  {
+    "revision": "4a9455bb4913c3f0b413",
+    "url": "/tracklistBuild/static/css/main.fb51016c.chunk.css"
+  },
+  {
+    "revision": "f4701418237bb2ba9fc6",
+    "url": "/tracklistBuild/static/js/2.6c845fc2.chunk.js"
+  },
+  {
+    "revision": "4a9455bb4913c3f0b413",
+    "url": "/tracklistBuild/static/js/main.58d2e993.chunk.js"
+  },
+  {
+    "revision": "1609d5845d2abe5cc8e4",
+    "url": "/tracklistBuild/static/js/runtime~main.1612caa0.js"
+  }
+]);
